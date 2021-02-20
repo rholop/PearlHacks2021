1d5a259f640200d0452a677676a727ecb2a7f40c
rholop <57956151+rholop@users.noreply.github.com>
2021-02-20 15:35:12 -0500

    Two Screens!

--- ph2021/lib/main.dart
+++ ph2021/lib/main.dart
@@ -5,9 +5,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  MyApp();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var gameScreenRouteName;
     return MaterialApp(
       title: 'PH 2021',
       theme: ThemeData(
@@ -22,10 +24,17 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    initialRoute: '/',
+    routes: <String, WidgetBuilder>{
+      '/': (BuildContext context) => MyHomePage(),
+      '/game': (BuildContext context) => SecondScreen(),
+    },
+      //home: MyHomePage(title: 'Starting Page'),
+
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -68,11 +77,11 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      //appBar: AppBar(
+      appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         //title: Text(widget.title),
-      //),
+      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -97,9 +106,34 @@ class _MyHomePageState extends State<MyHomePage> {
               'App Title',
               style: TextStyle(fontSize: 42),
             ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/game');
+              },
+              child: Text(
+                "Start Game",
+              ),
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: Text(
+          'Game here!',
+        ),
+      ),
+    );
+  }
+}
+
